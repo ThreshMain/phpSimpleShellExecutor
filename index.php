4e0b52d0c26aed42fb79360ebacdbfa38241b37c
ThreshMain <martinkos007@gmail.com>
2020-12-01 18:45:10 +0100

    Init commit

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php
+error_reporting(E_ALL);
+$hardCodedPassword="PASSWORD";
+$hardCodedUserName="UserName";
+
+$msg = "";
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = $_POST["name"];
+    $password = $_POST["password"];
+	 if ($name == '' || $password == '') {
+        $msg = "You must enter all fields";
+    } else {
+        if ($password === $hardCodedPassword && $name === $hardCodedUserName) {
+			echo 'Output: <pre style="border: 1px solid black">' . shell_exec('/bin/sudo -u root /home/root/discord-bot/clone.sh') . '</pre>';
+            exit;
+        }
+        $msg = "Username and password do not match";
+    }
+}
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>discussdesk.com - Login form in PHP mysql</title>
+<meta name="description" content=""/>
+<meta name="keywords" content=""/>
+<link href="style.css" rel="stylesheet" type="text/css">
+
+</head>
+<body>
+
+	<form name="frmregister"action="<?= $_SERVER['PHP_SELF'] ?>" method="post" >
+		<table class="form" border="0">
+
+			<tr>
+			<td></td>
+				<td style="color:red;">
+				<?php echo $msg; ?></td>
+			</tr> 
+			<tr>
+				<th><label for="name"><strong>Name:</strong></label></th>
+				<td><input class="inp-text" name="name" id="name" type="text" size="30" /></td>
+			</tr>
+			<tr>
+				<th><label for="name"><strong>Password:</strong></label></th>
+				<td><input class="inp-text" name="password" id="password" type="password" size="30" /></td>
+			</tr>
+			<tr>
+			<td></td>
+				<td class="submit-button-right">
+				<input class="send_btn" type="submit" value="Submit" alt="Submit" title="Submit" />
+				
+				<input class="send_btn" type="reset" value="Reset" alt="Reset" title="Reset" /></td>
+				
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
